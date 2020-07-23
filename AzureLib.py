672b74578654287b52bbbb6b8e6a668af8b2c757
waigitdas <genealogy@stumpf.org>
2020-07-23 14:17:40 -0500

    anonymize password

--- AzureLib.py
+++ AzureLib.py
@@ -2,7 +2,7 @@
 
 Neo4Server ="bolt://40.113.233.50:7687"  #"bolt://40.113.235.169:7687"   #"bolt://wai-neo4j.centralus.cloudapp.azure.com:7687"   #"bolt://graphdb.whoami.md:7687"   #"bolt://13.89.56.239:7687"
 Neo4UserName = "neo4j"
-Neo4Pswd ="WHIT!2020das"
+Neo4Pswd ="{your password}"
 Neo4jDatabase="omop"
 AzureBlob=""
 ImportDirectory=""
