6023d535cd3a7fdc02658ddfc81e4b3465480ee6
David A Stumpf, MD, PhD <67023119+waigitdas@users.noreply.github.com>
2020-08-27 15:48:56 -0500

    Update timer.py

--- timer.py
+++ timer.py
@@ -16,14 +16,4 @@ def getTicks():
     return time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
     return time.time()   
 
-    #clock_gettime()
-    #tics = ctypes.c_int64()
-    #freq = ctypes.c_int64()
-
-    ##get ticks on the internal ~2MHz QPC clock
-    #ctypes.windll.Kernel32.QueryPerformanceCounter(ctypes.byref(tics)) 
-    ##get the actual freq. of the internal ~2MHz QPC clock
-    #ctypes.windll.Kernel32.QueryPerformanceFrequency(ctypes.byref(freq))  
-
-    #t_us = tics.value*1e6/freq.value
-    #return t_us
+ 
